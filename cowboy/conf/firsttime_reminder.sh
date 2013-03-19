693af27cca249918fa16aa67299f43a1cb6419c2
Vania Smirnov <vaniaspeedy@gmail.com>
2013-03-18 22:51:57 -0700

    cloned my files, holman, and cowboy

---
+++ cowboy/conf/firsttime_reminder.sh
@@ -0,0 +1,12 @@
+# This file is sourced at the end of a first-time dotfiles install.
+shopt -s expand_aliases
+source ~/.bashrc
+
+# I'm forgetful. Just look at this repo's commits to see how many times I
+# forgot to setup Git and GitHub.
+
+cat <<EOF
+SSH Keys (if this is a server)
+ 1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
+ 2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+EOF
---
+++ holman/system/keys.zsh
@@ -0,0 +1,2 @@
+# Pipe my public key to my clipboard. Fuck you, pay me.
+alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
