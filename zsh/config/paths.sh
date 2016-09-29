000462099872c13ca0690009cbc5256d73d4d5f8
Ivan Smirnov <isgsmirnov@gmail.com>
2016-09-28 22:24:49 -0700

    add go path for osx

--- zsh/config/paths.sh
+++ zsh/config/paths.sh
@@ -1,5 +1,9 @@
 ## Define all paths here. This simplifies hunting for random path clobbering.
 
+GOLANG_OSX="/usr/local/go/bin"
+CUSTOM_HOME="$HOME/bin"
+SYSTEM="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
 # OS-dependent LaTeX paths.
 if [[ $OSTYPE == 'linux-gnu' ]]; then
 	TEX="/usr/texbin"
@@ -7,7 +11,4 @@ elif [[ $OSTYPE == darwin* ]]; then
 	TEX="/usr/local/texlive/2014/bin/universal-darwin"
 fi
 
-CUSTOM_HOME="$HOME/bin"
-SYSTEM="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-
-export PATH="$CUSTOM_HOME:$TEX:$SYSTEM"
+export PATH="$CUSTOM_HOME:$TEX:$GOLANG_OSX:$SYSTEM"
