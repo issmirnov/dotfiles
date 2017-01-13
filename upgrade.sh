4ade213985c5942568dd61949c6dd8345f63f9b0
Ivan Smirnov <isgsmirnov@gmail.com>
2017-01-13 15:30:47 -0800

    add upgrade script for removing vundle

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
-# nothing to do here, for now.
+# deprecate vundle
+mv vim/Vundle.vim vim/v
+git submodule deinit vim/Vundle.vim
+git rm vim/Vundle.vim
+rm -rf .git/modules/vim/Vundle.vim
+rm -rf vim/v
+
