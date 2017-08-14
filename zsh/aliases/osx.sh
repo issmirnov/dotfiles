b8ee236058393e52df663ce7442dd8f92da76fab
Ivan Smirnov <isgsmirnov@gmail.com>
2017-08-14 13:21:03 -0700

    add 'ch' - chrome history parser using fzf

--- zsh/aliases/osx.sh
+++ zsh/aliases/osx.sh
@@ -21,4 +21,19 @@ function osx_ins() {
 alias dfh='df -H -l | awk -F" " '"'"'{ $6="";  $7=""; $8=""; $10=""; print}'"'"' | column -t'
 
 
+# Browse history with fzf
+ch() {
+  local cols sep
+  cols=$(( COLUMNS / 3 ))
+  sep='{::}'
+
+  \cp -r ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+
+  sqlite3 -separator $sep /tmp/h \
+    "select substr(title, 1, $cols), url
+     from urls order by last_visit_time desc" |
+  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+}
+
 fi
