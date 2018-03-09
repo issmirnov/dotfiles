e5e2cf9736ec15d6800042229abc1703630b54fb
Ivan Smirnov <isgsmirnov@gmail.com>
2018-03-08 20:25:51 -0800

    set up symlink bin loader

--- zsh/config/paths.sh
+++ zsh/config/paths.sh
@@ -18,4 +18,16 @@ fpath=($HOME/.dotfiles/cheat/cheat.zsh $fpath)
 export GOPATH=$HOME/go
 GOLANG_BIN="/usr/local/go/bin:/usr/lib/go/bin"
 
+
+# Check for symlinks to directories in bin and 
+# append them to the path. This is useful when linking in
+# a suite such as flutter or android SDK.
+for elem in $USER_BIN/*; do
+    if [[ -L "$elem" && -d "$elem" ]]
+    then
+        USER_BIN=$USER_BIN:$elem
+    fi
+done
+
+# export final result
 export PATH="$USER_BIN:$DOT_BIN:$TEX:$GOPATH/bin:$GOLANG_BIN:$FZF_PREFIX/fzf/bin:$SYSTEM"
