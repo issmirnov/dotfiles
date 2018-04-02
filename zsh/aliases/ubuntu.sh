a231010bc660d0b76afb8f30c6f5faa60a0ea5f9
Ivan Smirnov <isgsmirnov@gmail.com>
2018-04-01 17:13:15 -0700

    remove old aliases

--- zsh/aliases/ubuntu.sh
+++ zsh/aliases/ubuntu.sh
@@ -3,14 +3,8 @@
 if [[ $OSTYPE == 'linux-gnu' ]]; then
 
 # sound
-alias maxvol='amixer -D pulse sset Master '"'"'100%='"'"''
-alias minvol='amixer -D pulse sset Master '"'"'0%='"'"''
-
-# launch free file sync with root rights
-alias ffs='gksudo FreeFileSync'
-
-# mount server
-alias mp='sudo mount -t cifs //puma/data /mnt/puma-data -o username=vania,file_mode=0777,dir_mode=0777'
+#alias maxvol='amixer -D pulse sset Master '"'"'100%='"'"''
+#alias minvol='amixer -D pulse sset Master '"'"'0%='"'"''
 
 # print stats about partitons, excluding tmpfs mounts
 alias dfh='df -h -x tmpfs -x devtmpfs | column -t'
