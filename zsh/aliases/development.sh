64cf1d6092854b2902d3c1df5101e049c83affda
Ivan Smirnov <isgsmirnov@gmail.com>
2018-06-05 22:56:56 -0700

    remove old aliases

--- zsh/aliases/development.sh
+++ zsh/aliases/development.sh
@@ -1,14 +1,8 @@
-## basics
-alias compile='gcc --std=c99 -o '
-
 ## python
 alias pt='python3 -m doctest'
 alias pv='python3 -m doctest -v'
 alias pi='python3 -i'
 alias pserver="python -m SimpleHTTPServer"
-
-## projects
-alias p="cd /Volumes/Data/Personal/Projects"
 
 ## ansible
 alias ap='ansible-playbook'
