63d7a42070ba315c69c729f86e2a519b177d643a
Ivan Smirnov <isgsmirnov@gmail.com>
2018-07-19 21:50:12 -0700

    add ansible alias

--- zsh/aliases/development.sh
+++ zsh/aliases/development.sh
@@ -1,10 +1,11 @@
 ## python
-alias pt='python3 -m doctest'
-alias pv='python3 -m doctest -v'
 alias pi='python3 -i'
 alias pserver="python -m SimpleHTTPServer"
 
 ## ansible
 alias ap='ansible-playbook'
 alias agi='ansible-galaxy install -r requirements.yml --ignore-errors --keep-scm-meta'
+alias agif='agi --force'
+
+# vagrant
 alias vup='vagrant up'
