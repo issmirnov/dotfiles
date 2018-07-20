89719972b7cdee9a123a29a01c943cb5732bd7b3
Ivan Smirnov <isgsmirnov@gmail.com>
2018-07-20 11:05:43 -0700

    add "highlight" utility
    This will allow me to do:
    - echo a b c | highlight red a
    
    Credit: https://github.com/kepkin/dev-shell-essentials/blob/master/highlight.sh

--- zsh/aliases/utilities.sh
+++ zsh/aliases/utilities.sh
@@ -62,3 +62,26 @@ function rm+ () {
 
 #### end zsh-tips ####
 
+
+# highlight - helper that adds color decorators to text
+# credit: https://github.com/kepkin/dev-shell-essentials/blob/master/highlight.sh
+highlight() {
+    if [ "$#" -lt 2 ]; then
+        echo "Usage: $0 black|red|green|yellow|blue|magenta|cyan your text here"
+        return 2
+    fi
+
+    declare -A fg_color_map
+    fg_color_map[black]=30
+    fg_color_map[red]=31
+    fg_color_map[green]=32
+    fg_color_map[yellow]=33
+    fg_color_map[blue]=34
+    fg_color_map[magenta]=35
+    fg_color_map[cyan]=36
+
+    fg_c=$(echo -e "\e[1;${fg_color_map[$1]}m")
+    c_rs=$'\e[0m'
+    shift
+    sed -u s"/$*/$fg_c\0$c_rs/g"
+}
