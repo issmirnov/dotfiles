f0e542a3f0ca4bd75d221f77157e176a8786d276
Ivan Smirnov <isgsmirnov@gmail.com>
2018-07-28 14:23:04 -0700

    add alias to pull ansible variables

--- zsh/aliases/development.sh
+++ zsh/aliases/development.sh
@@ -6,6 +6,10 @@ alias pserver="python -m SimpleHTTPServer"
 alias ap='ansible-playbook'
 alias agi='ansible-galaxy install -r requirements.yml --ignore-errors --keep-scm-meta'
 alias agif='agi --force'
+# get ansible facts
+af() {
+  ansible localhost -m setup | sed 's@localhost | SUCCESS => {@{@g' | jq ".ansible_facts.$1"
+}
 
 # vagrant
 alias vup='vagrant up'
