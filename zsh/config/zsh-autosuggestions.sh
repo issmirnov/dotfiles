6d54d8ae9e77c140b987e8a72faa2b6005270c6d
Ivan Smirnov <isgsmirnov@gmail.com>
2018-08-19 23:06:17 -0700

    add better highlighting

--- zsh/config/zsh-autosuggestions.sh
+++ zsh/config/zsh-autosuggestions.sh
@@ -1,4 +1,7 @@
 export ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
 
+# async, for speed
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+
 # spectrum_ls from oh-my-zsh will list all codes
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=010'
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=010'
