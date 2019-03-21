2c0b0854549cc110735714cf35f83a24da4851bf
Ivan Smirnov <isgsmirnov@gmail.com>
2019-03-20 23:14:06 -0700

    add bat config

---
+++ zsh/config/bat.zsh
@@ -0,0 +1,6 @@
+# Configure https://github.com/sharkdp/bat
+export BAT_CONFIG_PATH="$HOME/.dotfiles/bat/bat.conf"
+
+# define pager. Note that even though this is defined in the 
+# bat config file, for some reason we still have to set it here.
+export BAT_PAGER="less -RF"
