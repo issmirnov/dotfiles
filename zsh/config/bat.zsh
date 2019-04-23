0be3eb4b63b62a4ac1b1a1fead1cf73b48b83b42
Ivan Smirnov <isgsmirnov@gmail.com>
2019-04-23 16:29:08 -0700

    Update BAT_PAGER
    There was a bug with "show" where files would be displayed as empty text. Drilling down I discovered that "less -FR" would quit out and show nothing if the file was less than one page long. Apparently adding -X and -S solves this issue.

--- zsh/config/bat.zsh
+++ zsh/config/bat.zsh
@@ -3,4 +3,4 @@ export BAT_CONFIG_PATH="$HOME/.dotfiles/bat/bat.conf"
 
 # define pager. Note that even though this is defined in the 
 # bat config file, for some reason we still have to set it here.
-export BAT_PAGER="less -RF"
+export BAT_PAGER="less -RFXS"
