a9c45a9246b3f809802042a40370a478a8a56782
Ivan Smirnov <isgsmirnov@gmail.com>
2021-03-27 23:18:52 -0600

    enable reporttime

---
+++ zsh/config/zsh.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+# Core ZSH config params
+#
+# Report CPU usage for commands running longer than 10 seconds
+# Info: https://nuclearsquid.com/writings/reporttime-in-zsh/
+REPORTTIME=10
