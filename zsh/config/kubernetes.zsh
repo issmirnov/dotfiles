c16db9456b173811dabcf9d29904413a76d064bb
Ivan Smirnov <isgsmirnov@gmail.com>
2021-08-23 16:31:57 -0600

    add kubecolor

--- zsh/config/kubernetes.zsh
+++ zsh/config/kubernetes.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
+
+# use https://github.com/dty1er/kubecolor
+command -v kubecolor >/dev/null 2>&1 && alias kubectl="kubecolor"
