0f7fb39e4b50b4ce1d3dde0e339325c63aae8c0b
Ivan Smirnov <isgsmirnov@gmail.com>
2022-03-07 19:18:28 +0100

    rename m alias

--- zsh/aliases/elude.zsh
+++ zsh/aliases/elude.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Pop to monorepo
-alias m='cd /mnt/Spark/Projects/elude/monorepo'
+alias mono='cd /mnt/Spark/Projects/elude/monorepo'
 alias mt='m; cd k8s/helm/tools'
 alias ms='m; cd k8s/helm/services'
 alias mg='m; cd go'
