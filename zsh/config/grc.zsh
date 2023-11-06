0095562782f641d4690710820b7e0d0804e427cc
Ivan Smirnov <isgsmirnov@gmail.com>
2023-11-06 09:40:14 -0700

    Update GRC script to work with docker commands
    When we checked https://github.com/garabik/grc/tree/master/colourfiles
    it turns out that there are docker* files available. The GRC script
    has been patched to detect docker commands, and check for valid config
    files.
    
    This makes commands like "docker ps" or "docker pull" much easier to
    parse.

--- zsh/config/grc.zsh
+++ zsh/config/grc.zsh
@@ -6,15 +6,30 @@ _grc_injector(){
         local f
 	for f in $(ls ~/.dotfiles/grc/conf*); do
 	    local prog=${f:e}
+
+	    # don't set this to local, so that we can update this inside the "if" block
+	    # we explicitly unset at the end of the function to clean up the env.
+	    progmatch=${f:e}
+
+	    # echo "PROG: $prog"
+	    # docker patch, so that "dockerpull" and "dockerimages" config files get picked up
+	    if [[ $prog == docker* ]]; then
+	      # Add a space after the "docker" prefix
+	      # save to second variable, as we need to match on the command with the space
+	      # but pass the non-spaced version to the GRC prog
+	      progmatch="docker ${prog#docker}"
+	    fi
+	    # echo "PROG after transform $progmatch"
 	    # regex: match oneof:
 	    #  - start of line
 	    #  - whitespace
 	    #  - sudo
 	    # then check for existence of program (determined by grc file suffix) and make sure we don't infintely append to zsh BUFFER
-	    if [[ "$BUFFER" =~ "(^|[/\w\.]+/|sudo\s+)$prog(\s|$)\s?" && ! "$BUFFER" =~ "grcat conf*" ]]; then
+	    if [[ "$BUFFER" =~ "(^|[/\w\.]+/|sudo\s+)$progmatch(\s|$)\s?" && ! "$BUFFER" =~ "grcat conf*" ]]; then
 		BUFFER=$BUFFER" | grcat conf.$prog"
 		break
 	    fi
+	    unset progmatch
 	done
     fi
     zle .accept-line
