3ef2e7127c6f9b86e9dd4e28c13fa03cbd68e784
Ivan Smirnov <isgsmirnov@gmail.com>
2023-11-27 00:40:10 -0700

    Add sudo space hack from arch linux wiki

--- zsh/config/zsh.zsh
+++ zsh/config/zsh.zsh
@@ -4,3 +4,9 @@
 # Report CPU usage for commands running longer than 10 seconds
 # Info: https://nuclearsquid.com/writings/reporttime-in-zsh/
 REPORTTIME=10
+
+# Aliases in Zsh and Bash are normally only expanded for the first
+# word in a command. This means that your aliases will not normally
+# get expanded when running the sudo command. One way to make the
+# next word expand is to make an alias for sudo ending with a space.
+alias sudo='sudo '
